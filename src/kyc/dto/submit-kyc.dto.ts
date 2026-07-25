91d6f1397a42b40d427d776bf39b1b689b6ba6dc
ahmadogo <abdulkareemahmad413@gmail.com>
2026-07-25 19:33:05 +0100

    nothing to commit

--- src/kyc/dto/submit-kyc.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
-import { KycDocumentType } from '../schemas/kyc.schema';
-
-export class SubmitKycDto {
-  @IsEnum(KycDocumentType)
-  @IsNotEmpty()
-  documentType!: KycDocumentType;
-}

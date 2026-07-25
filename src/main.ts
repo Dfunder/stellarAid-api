6a3538806fc7052fe71bdb6c599eb2ba240cbbcb
Ahmad Abdulkareem <a-abdulkareem@leadway.com>
2026-07-25 20:08:15 +0100

    Configure CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.NEXT_PUBLIC_FRONTEND_URL,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
